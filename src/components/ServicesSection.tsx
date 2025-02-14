b6c4ebd3c0bfea4cdfab0159961f5f44d46f12ea
JayChuang8 <jaychuang1@gmail.com>
2025-02-13 21:21:40 -0700

    Change service item images

--- src/components/ServicesSection.tsx
+++ src/components/ServicesSection.tsx
@@ -10,28 +10,28 @@ function ServicesSection() {
         <ul className="services-section-items">
           <CardItem
             color="#F5F5DC"
-            imageSrc="images/icon-acupuncture.svg"
+            imageSrc="images/accupuncture.jpg"
             label="Learn More"
             path="/accupuncture"
             text="Accupuncture"
           />
           <CardItem
             color="#D8CFC4"
-            imageSrc="images/icon-massage.svg"
+            imageSrc="images/massage.jpg"
             label="Learn More"
             path="/massage"
             text="Massage"
           />
           <CardItem
             color="#DDE6D5"
-            imageSrc="images/icon-herbs.svg"
+            imageSrc="images/herbs.jpg"
             label="Learn More"
             path="/tcmHerbs"
             text="TCM Herbs"
           />
           <CardItem
             color="#EAD8C0"
-            imageSrc="images/icon-cupping.png"
+            imageSrc="images/cupping.jpg"
             label="Learn More"
             path="/cupping"
             text="Cupping"
