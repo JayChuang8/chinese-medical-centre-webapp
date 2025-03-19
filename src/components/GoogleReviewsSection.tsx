75eb1cdcfb0b5cf4cd4df8054867f402f9aebd74
JayChuang8 <jaychuang1@gmail.com>
2025-03-19 13:59:35 -0600

    Add widget id for google reviews

--- src/components/GoogleReviewsSection.tsx
+++ src/components/GoogleReviewsSection.tsx
@@ -3,7 +3,7 @@ import "./GoogleReviewsSection.css";
 import { ReactGoogleReviews } from "react-google-reviews";
 import "react-google-reviews/dist/index.css";
 
-const featurableId = "example";
+const featurableId = "ec2f66e3-0aa7-4a91-9744-e08f5ebee258";
 function GoogleReviewsSection() {
   return (
     <div className="google-reviews-section">
